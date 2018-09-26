51692c6fa41bab547e662dfdeafc5a35a0d24f3c
Jihae Kim <jihaekim@buffalo.edu>
2018-09-26 17:30:06 -0400

    key

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -5,7 +5,7 @@ class RestaurantController < ApplicationController
     def index
         @lat = params[:lat]
         @lng= params[:lng]
-        @client = GooglePlaces::Client.new('AIzaSyAMfRndh8oVyNMwvoOk1B3sMJwiYTHs6V0')
+        @client = GooglePlaces::Client.new('apikey')
         @spot = @client.spots(@lat, @lng, :radius => 5000, :types => ['restaurant'], :exclude => ['cafe','bakery'])
         @cafes = @client.spots(@lat, @lng, :types => ['cafe','bakery'])
         
@@ -14,7 +14,7 @@ class RestaurantController < ApplicationController
     def info
         @lat = params[:lat]
         @lng= params[:lng]
-        @client = GooglePlaces::Client.new('AIzaSyAMfRndh8oVyNMwvoOk1B3sMJwiYTHs6V0')
+        @client = GooglePlaces::Client.new('apikey')
         @place = params[:place]
        @spot =  @client.spot(params[:place], :detail => true)
     end
